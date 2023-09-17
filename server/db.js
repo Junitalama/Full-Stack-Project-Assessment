c5764b65c16f251f52991787d1a0ed76a5a6f007
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-09-17 23:53:52 +0100

    db.js commented out

--- server/db.js
+++ server/db.js
@@ -1,17 +1,1 @@
-const { Pool } = require("pg");
-require("dotenv").config();
-
-
-const pool = new Pool({
-  user: process.env.USERNAME,
-  host: process.env.HOSTNAME,
-  database: process.env.DATABASE_NAME,
-  password: process.env.PASSWORD,
-  port: process.env.PORT,
-  ssl: true,
-});
-
-module.exports = pool;
-  
-  
-
+// node
