03f313f01c6265cf8d0b9980c670d7a1346d6c24
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-09-18 11:30:57 +0100

    done get,post and delete query

--- server/server.js
+++ server/server.js
@@ -28,3 +28,32 @@ app.get("/:id", (req, res) => {
   .then(result => res.status(200).json(result.rows))
   .catch(err => res.send(err));
 });
+
+
+
+app.post("/", (req, res) => {
+  const { title, url } = req.body;
+  db.query(
+    "insert into videos (title, url) values ($1, $2) returning id", [title, url]
+  )
+  .then(result => res.status(200).json(result.rows[0]))
+  .catch(err => res.send(err));
+});
+
+app.post("/rating", (req, res) => {
+  const {id, rating} = req.body;
+  db.query('update videos set rating = $1 where id = $2', [rating, id])
+  .then(()=> res.sendStatus(204))
+  .catch(err => res.send(err));
+});
+
+//delete
+app.delete("/:id", (req, res) => {
+  let idToDelete = Number(req.params.id);
+  db.query("delete from videos where id = $1", [idToDelete]).then(() =>
+    res.status(200).json({})
+  )
+  .catch(err => res.send(err));
+});
+
+
