3687513aa84fa0b00df12bad565c8b3d09866ccf
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-09-20 01:48:35 +0100

    added styling

--- client/src/components/MainCard.jsx
+++ client/src/components/MainCard.jsx
@@ -7,7 +7,7 @@ const MainCard = ({ video, onVote, onDelete }) => {
     <div className="video">
       <h2>{video.title}</h2>
       <iframe
-        width="560"
+        width="450"
         height="315"
         src={`https://www.youtube.com/embed/${videoId}`}
         title="YouTube video player"
