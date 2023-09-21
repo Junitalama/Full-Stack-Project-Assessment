8a657b63bd77ff165c72aa7d516d047fd5cccd53
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-09-21 08:45:37 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
-app.use(cors());
+app.use(cors({AllowedHeaders: ['Content-Type', 'Authorization']}));
 const { Pool } = require("pg");
 app.use(bodyParser.urlencoded({ extended: false }));
 const db = new Pool({
